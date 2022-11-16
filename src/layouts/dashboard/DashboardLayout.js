0024aaaec24d666e48747b2722e8916264d038b8
bilal-mustafa10 <bilalmustafa041@icloud.com>
2022-11-16 14:44:24 +0000

    initial commit

---
+++ src/layouts/dashboard/DashboardLayout.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+//
+import Header from './header';
+
+// ----------------------------------------------------------------------
+
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 92;
+
+const StyledRoot = styled('div')({
+  display: 'flex',
+  minHeight: '100%',
+  overflow: 'hidden',
+});
+
+const Main = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  overflow: 'auto',
+  minHeight: '100%',
+  paddingTop: 0,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: 50,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
+
+// ----------------------------------------------------------------------
+
+export default function DashboardLayout() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <StyledRoot>
+     {/* <Header onOpenNav={() => setOpen(true)} /> */}
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledRoot>
+  );
+}
